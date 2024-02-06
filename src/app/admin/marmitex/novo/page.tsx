720e5974be8ce061cf7e7102c126505163743d1d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-02-06 19:05:14 -0300

    :construction: trabalhando ...

--- src/app/admin/marmitex/novo/page.tsx
+++ src/app/admin/marmitex/novo/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { TipoItemDocument } from "@/model/TipoItemConsumivel";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function NovoMarmitexPage() {
 
   const [tipoItems, setTipoItems] = useState<TipoItemDocument[]>();
+  const [descricao, setDescricao] = useState<string>("");
+  const [descricaoBotao, setDescricaoBotao] = useState<string>("Proximo");
 
   useEffect(() => {
     fetchTipoItems();
@@ -27,6 +30,10 @@ export default function NovoMarmitexPage() {
     )
   }
 
+  function step() {
+    setDescricaoBotao("Ola proximo")
+  }
+
   return (
     <div className="flex flex-col justify-center p-3">
 
@@ -35,13 +42,19 @@ export default function NovoMarmitexPage() {
         <button className="btn btn-primary">Novo marmitex</button>
       </div>
 
-      <ul className="steps">
-        <li className="step step-primary">Descrição</li>
-        {tipoItems.map((tipo) => (
-          <li key={tipo._id} className="step">{tipo.descricao}</li>
-        ))}
-        <li className="step">Salvar</li>
-      </ul>
+
+
+      <div className="flex justify-center">
+
+        <ul className="steps steps-vertical text-xs sm:text-xl sm:steps-horizontal">
+          <li className="step step-primary">Descrição</li>
+          {tipoItems.map((tipo) => (
+            <li key={tipo._id} className="step">{tipo.descricao.split(" ")[0]}</li>
+          ))}
+          <li className="step">Salvar</li>
+        </ul>
+      </div>
+
 
       <div className="flex flex-col items-center mt-4 space-y-4">
         <div>
@@ -50,13 +63,13 @@ export default function NovoMarmitexPage() {
             <div className="label">
               <span className="label-text">Descrição</span>
             </div>
-            <input type="text" placeholder="Nome marmitex ..." className="input input-bordered w-full max-w-xs" />
+            <input type="text" value={descricao} onChange={(e) => setDescricao(e.target.value)} placeholder="Nome marmitex ..." className="input input-bordered w-full max-w-xs" />
 
           </label>
         </div>
-        <div>
-
-          <button className="btn btn-secondary">Proximo</button>
+        <div className="flex flex-col space-y-3">
+          <button onClick={step} className="btn btn-secondary">{descricaoBotao}</button>
+          <Link href={"/admin/marmitex"} className="btn btn-link">Cancelar</Link>
         </div>
       </div>
     </div>
