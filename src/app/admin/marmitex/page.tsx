eb327322504160f079f9e3e0e9ecd284b592d078
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-02-20 21:39:26 -0300

    :sparkles: list marmitex

--- src/app/admin/marmitex/page.tsx
+++ src/app/admin/marmitex/page.tsx
@@ -1,50 +1,68 @@
+"use client";
+
+import { URL_API_TIPO_MARMITEX } from "@/constants/constants";
+import { TipoMarmitexDocument } from "@/model/TipoMarmitex";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function MarmitexPage() {
-  return (
-    <div className="flex flex-col justify-center p-3">
 
-      <div className="flex justify-center mb-4">
+  const [tipoMarmitex, setTipoMarmitex] = useState<TipoMarmitexDocument[]>();
 
-        <Link href={"/admin/marmitex/novo"} className="btn btn-primary">Novo marmitex</Link>
+  useEffect(() => {
+    fetchTipoItems();
+  }, []);
+
+  function fetchTipoItems() {
+    fetch(URL_API_TIPO_MARMITEX).then((res) =>
+      res.json().then((mamitex) => {
+        setTipoMarmitex(mamitex);
+      })
+    );
+  }
+
+  if (tipoMarmitex) {
+    return (
+      <div className="flex flex-col justify-center p-3 items-center">
+
+        <div className="flex justify-center mb-4">
+
+          <Link href={"/admin/marmitex/novo"} className="btn btn-primary">Novo marmitex</Link>
+        </div>
+
+        <div className="p-4 rounded-md border-2 border-blue-400">
+          {tipoMarmitex.map((tipo) => (
+            <div>
+              <h2 className="mb-1 text-3xl text-center">{tipo.descricao}</h2>
+              <h2 className="text-sm text-center font-semibold text-secondary">{tipo.ativo ? "Ativo" : "NÃO"}</h2>
+              <div className="mb-4">
+                {tipo.configuracoes.map((config, index) => (
+                  <div key={`config-${index}`}>
+                    <ul>
+                      <li className="space-x-3">
+                        <span className="my-1 badge badge-lg badge-primary">
+                          {config.quantidade}
+                        </span>
+                        
+                        <span className="font-light">
+                          {config.tipo.descricao}
+                        </span>
+
+                      </li>
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Descrição</th>
-              <th>Job</th>
-              <th>Preço</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <td>Cy Ganderton</td>
-              <td>Quality Control Specialist</td>
-              <td>Blue</td>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <th>2</th>
-              <td>Hart Hagerty</td>
-              <td>Desktop Support Technician</td>
-              <td>Purple</td>
-            </tr>
-            {/* row 3 */}
-            <tr>
-              <th>3</th>
-              <td>Brice Swyre</td>
-              <td>Tax Accountant</td>
-              <td>Red</td>
-            </tr>
-          </tbody>
-        </table>
+    );
+  } else {
+    return (
+      <div>
+        <h2> Carregando ...</h2>
       </div>
-    </div>
-  );
+    )
+  }
 }
--- src/app/api/marmitex/tipo/route.ts
+++ src/app/api/marmitex/tipo/route.ts
@@ -2,17 +2,21 @@ import { ITipoMarmitex } from "@/interfaces/ITipoMarmitex";
 import { TipoMarmitex } from "@/model/TipoMarmitex";
 import mongoose from "mongoose";
 import { NextRequest } from "next/server";
+import path from "path";
 
-// export async function GET(req: NextRequest) {
-//   mongoose.connect(process.env.MONGODB_URI as string);
+export async function GET(req: NextRequest) {
+  mongoose.connect(process.env.MONGODB_URI as string);
 
-//   const cardapio = await Cardapio.find().populate({
-//     path: "itens",
-//     populate: { path: "tipo", select: '_id descricao exibirPreco' }
-//   });
+  const tipoMarmitex = await TipoMarmitex.find().populate({
+    path: "configuracoes",
+    populate: {
+      path: "tipo",
+      select: "descricao imagem"
+    }
+  });
 
-//   return Response.json(cardapio, { status: 200 });
-// }
+  return Response.json(tipoMarmitex, { status: 200 });
+}
 
 export async function POST(req: NextRequest) {
   mongoose.connect(process.env.MONGODB_URI as string);
